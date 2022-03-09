f52b5a23f61306ce789922561a3867d6d959654d
Vasileios Vavourakis <vasvav@gmail.com>
2022-03-09 09:24:17 +0200

    Allow user to choose which model to simulate (prospectively)

--- src/main.C
+++ src/main.C
@@ -6,7 +6,6 @@ using namespace libMesh;
 
 PerfLog plog("rdcFEs");
 
-
 extern void adpm (LibMeshInit & );
 
 int main (int argc, char* argv[])
@@ -14,8 +13,13 @@ int main (int argc, char* argv[])
   LibMeshInit init(argc, argv);
   GetPot command_line(argc, argv);
 
+  std::string model;
+  if (command_line.search(1, "-m"))
+    model = command_line.next(model);
+  else return 1;
+
   // Alzheimer's disease progression model
-  adpm(init);
+  if ("adpm"==model) adpm(init);
 
   libMesh::out << std::endl << plog.get_log() << std::endl;
   // ...done
