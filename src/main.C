9c12b72f40e58cc70a0c2d1b86a1edaced86e174
Vasileios Vavourakis <vasvav@gmail.com>
2022-07-16 23:19:10 +0300

    A bit of tidy up in 'main' function

--- src/main.C
+++ src/main.C
@@ -15,17 +15,18 @@ int main (int argc, char* argv[])
   LibMeshInit init(argc, argv);
   GetPot command_line(argc, argv);
 
-  std::string model;
+  std::string s;
   if (command_line.search(1, "-m"))
-    model = command_line.next(model);
+    {
+      s = command_line.next(s);
+      // Alzheimer's disease progression model
+      if ("adpm"==s) adpm(init);
+      // PIHNA cancer model
+      else if ("pihna"==s) pihna(init);
+      // radiation-induced pulmonary fibrosis model
+      else if ("ripf"==s) ripf(init);
+    }
   else return 1;
-
-  // Alzheimer's disease progression model
-  if ("adpm"==model) adpm(init);
-  // PIHNA cancer model
-  else if ("pihna"==model) pihna(init);
-  // radiation-induced pulmonary fibrosis model
-  else if ("ripf"==model) ripf(init);
 
   libMesh::out << std::endl << plog.get_log() << std::endl;
   // ...done
